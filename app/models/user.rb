d6b0a2713a94a8312cb03814021e2e0eb8140a8c
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-09-16 14:54:45 -0700

    Initial commit. Created User model.

---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :email, :session_token, presence: true
+  validates :email, uniquness: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialze :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = self.find_by(email: email)
+    user.try(:is_password?, password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    update(session_token: generate_session_token)
+    session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+end
